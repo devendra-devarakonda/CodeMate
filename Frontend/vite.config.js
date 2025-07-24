5ea4f7b9391f6a6ca0a3ce2bb138b06281ed943d
devendra-devarakonda <devdevarkonda677@gmail.com>
2025-07-24 22:25:46 +0530

    updated changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  base: '/',
   server: {
     proxy: {
       '/api': {
@@ -11,4 +15,5 @@ export default defineConfig({
       },
     },
   },
+  
 })
